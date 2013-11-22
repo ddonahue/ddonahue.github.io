646ae419d4a1d5cfc3bec152ee030a58266a64ad
Dan Donahue <ddonahue215@gmail.com>
2013-11-22 17:05:43 -0500

    Update Google Analytics.

--- javascripts/google.analytics.js
+++ javascripts/google.analytics.js
@@ -4,4 +4,5 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-45243911-1', 'dandonahue.net');
+ga('require', 'linkid', 'linkid.js');
 ga('send', 'pageview');
