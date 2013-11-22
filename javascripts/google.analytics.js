5116da634ccc74bc879c74dfbc40d48d937c8ca5
Dan Donahue <ddonahue215@gmail.com>
2013-11-22 16:59:12 -0500

    Add new Google Analytics code with correct URL.

--- javascripts/google.analytics.js
+++ javascripts/google.analytics.js
@@ -3,6 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-45243911-1', 'ddonahue.github.io');
+ga('create', 'UA-45243911-1', 'dandonahue.net');
 ga('send', 'pageview');
-
